558d9f22a858171f769ee7b6a1dce260cdab8fbc
JuanMMaidana <juan.maidana@correo.ucu.edu.uy>
2022-09-18 17:58:29 -0300

    navbar-sup

--- ucuGram/src/app/app.module.ts
+++ ucuGram/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarSupComponent } from './nav-bar-sup/nav-bar-sup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarSupComponent
   ],
   imports: [
     BrowserModule
