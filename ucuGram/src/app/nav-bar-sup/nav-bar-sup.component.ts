3d1fb3164ac6f70561e196e046e10824dead8af3
JuanMMaidana <juan.maidana@correo.ucu.edu.uy>
2022-09-21 18:41:01 -0300

    Haciendo funcioar navBar

--- ucuGram/src/app/nav-bar-sup/nav-bar-sup.component.ts
+++ ucuGram/src/app/nav-bar-sup/nav-bar-sup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar-sup',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class NavBarSupComponent implements OnInit {
 
   constructor() { }
+
+  @Input() navtext?: string;
 
   ngOnInit(): void {
   }
